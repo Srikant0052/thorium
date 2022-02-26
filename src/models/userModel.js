2a89fe8f0c809997dd310b075444eccf1e34ccae
Srikant0052 <srikant0052@gmail.com>
2022-02-26 13:47:56 +0530

    book-schema-assignment-W3D5

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,30 +1,20 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    category: {
         type: String,
-        unique: true,
-        required: true
+        enum: ["myth", "romance", "novel", "fiction", "biography", "firyTale",
+         "drama", "history", "poems", "nonFiction","wetern" ]
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    year: Number,
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('User', bookSchema) //users
 
+// Create a bookSchema with bookName, authorName, category and year . 
+// Create same 2 api's for books i.e. : 1 api to create a new book and another api to get the list of all books. 
 
 
 // String, Number
