fce460a17c5a5d99f2404adc5077d5187d9c63ff
Srikant0052 <srikant0052@gmail.com>
2022-02-26 22:15:54 +0530

    git ignorfile add

---
+++ src/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    category: {
+        type: String,
+        enum: ["myth", "romance", "novel", "fiction", "biography", "firyTale",
+         "drama", "history", "poems", "nonFiction","western" ]
+    },
+    year: Number,
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', bookSchema) //users
+
+// Create a bookSchema with bookName, authorName, category and year . 
+// Create same 2 api's for books i.e. : 1 api to create a new book and another api to get the list of all books. 
+
+
+// String, Number
+// Boolean, Object/json, array
